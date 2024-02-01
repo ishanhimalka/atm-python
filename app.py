6f8590a7241964b1484a03ef6cd9e6586fc4b3ce
ishanhimalka <ishanhimalka1@gmail.com>
2024-02-02 00:35:56 +0100

    get exticing account

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import random
-import sys
 from src.databse import MySQLConnection
 from src.newaccount import NewAccount
 
@@ -7,12 +5,19 @@ connection_object = MySQLConnection.connect()
 cursor = connection_object.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS atm (name VARCHAR(255), account_number VARCHAR(255), balance INT(10))")
+class ATM:
+    existing_account = input("Have an account?(y/n):")
+    if existing_account == "y":
+        account = input("Enter your Account Number: ")
+        cursor.execute("SELECT * FROM atm WHERE account_number = %s", (account,))
+        result = cursor.fetchone()
+        if result:
+            print(result)
+        else:
+            print("Invalid Account Number!")
+    elif existing_account == "n":
+        NewAccount()
+    else:
+        print("Wrong command!  Enter 'y' for yes and 'n' for NO.\n")
 
-existing_account = input("Have an account?(y/n):")
-if existing_account == "y":
-     print("adf")
-elif existing_account == "n":
-    NewAccount()
-else:
-    print("Wrong command!  Enter 'y' for yes and 'n' for NO.\n")
-
+ 
--- src/newaccount.py
+++ src/newaccount.py
@@ -4,7 +4,7 @@ connection_object = MySQLConnection.connect()
 
 cursor = connection_object.cursor()
 class NewAccount():
-    name = input("Enter your name2: ")
+    name = input("Enter your name: ")
     account_number = input("Enter your account number: ")
     balance = 0
     cursor.execute("INSERT INTO atm (name, account_number, balance) VALUES (%s, %s, %s)", (name, account_number, balance))
